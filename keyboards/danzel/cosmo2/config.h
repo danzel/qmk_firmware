4a5f4ebda480ac74ace962be48ff8bf4103448e2
Dave Leaver <danzel@localhost.geek.nz>
2025-07-14 15:23:41 +1200

    Add new keeb, no keymap

---
+++ keyboards/danzel/cosmo2/config.h
@@ -0,0 +1,6 @@
+//https://docs.qmk.fm/drivers/serial#setup-2
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN GP0     // USART TX pin
+#define SERIAL_USART_RX_PIN GP1     // USART RX pin
+
+#define SPLIT_WATCHDOG_ENABLE
